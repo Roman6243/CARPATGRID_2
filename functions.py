4f03ae9cb9934fbd0d5f7614a264a797b87325e3
Roman6243 <mukhovskiy2012@gmail.com>
2021-02-16 19:25:44 +0100

    make small cosmetic changes

--- functions.py
+++ functions.py
@@ -7,3 +7,24 @@ def plot_hm(df, value, vmin, vmax, month):
     m_name = months[months['order'] == month]['month'].tolist()[0]
     plt.savefig('maps/'+ str(month) + "_" + m_name + "_" + value + '.png')
     print("save chart for month - " + m_name)
+
+def extract_d(f_input, f_output, param):
+    if os.path.isfile(f_output):
+        print("File " + f_input.split('\\')[1] +  " exist")
+    else:
+        print("File not exist")
+        with open(f_input) as fin, open(f_output, 'w') as fout:
+            for line in fin:
+                fout.write(";".join(line.split()) + "\n")
+            print("File " + f_input.split('\\')[1] +  " saved successfully!")
+
+    df = pd.read_csv(f_output, delimiter=';')
+    print('File ' + f_input.split('\\')[1] +  ' read successfully!')
+    cols = df.columns
+    df.reset_index(inplace=True)
+
+    df = pd.melt(df, id_vars=['level_0', 'level_1', 'level_2'], value_vars = cols, value_name = param, var_name='index')
+    df = df.rename(columns={"level_0": "year", "level_1": "month", "level_2": "day"})
+    df = df.astype( {'year': int, 'month': int, 'day': int})
+    print('Function run successfully!')
+    return df
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import ttest_1samp
 from functions import plot_hm
-import geopandas as gpd
 
 # turn off the ineractive mode for pop up plot windows
 plt.ioff()
@@ -34,34 +33,12 @@ alt = pd.read_csv(f_out, delimiter=';')
 alt["index"] = alt["index"].astype(str)
 
 
-
 # to write data in different year batch
 years = [[1961, 1970], [1971, 1980], [1981, 1990], [1991, 2000], [2001, 2010]]
 y_frames = pd.DataFrame(years, columns=['start', 'end'])
 for i in range(1,5):
     start = y_frames['start'][i]
     end = y_frames['end'][i]
-
-    def extract_d(f_input, f_output, param):
-        if os.path.isfile(f_output):
-            print("File " + f_input.split('\\')[1] +  " exist")
-        else:
-            print("File not exist")
-            with open(f_input) as fin, open(f_output, 'w') as fout:
-                for line in fin:
-                    fout.write(";".join(line.split()) + "\n")
-                print("File " + f_input.split('\\')[1] +  " saved successfully!")
-
-        df = pd.read_csv(f_output, delimiter=';')
-        print('File ' + f_input.split('\\')[1] +  ' read successfully!')
-        cols = df.columns
-        df.reset_index(inplace=True)
-
-        df = pd.melt(df, id_vars=['level_0', 'level_1', 'level_2'], value_vars = cols, value_name = param, var_name='index')
-        df = df.rename(columns={"level_0": "year", "level_1": "month", "level_2": "day"})
-        df = df.astype( {'year': int, 'month': int, 'day': int})
-        print('Function run successfully!')
-        return df
 
     rg = extract_d(f_input = "CARPATGRID_RG\\CARPATGRID_RG_D.ser", f_output = "CARPATGRID_RG\\CARPATGRID_RG_D.csv", param = 'rg12')
     rg = rg.merge(alt, how='left', on=['index'])
@@ -106,26 +83,21 @@ utci_2001_2010 = pd.read_pickle('results/df2001_2010.pkl')
 utci_list = [utci_1961_1970, utci_1971_1980, utci_1981_1990, utci_1991_2000, utci_2001_2010]
 utci = pd.concat(utci_list)
 
-# count number of days with different utci
-# utci.loc[(utci['utci'] < -40), 'tsc'] = 'EC'
-# utci.loc[(utci['utci'] > -40) & (utci['utci'] <= -27.1), 'tsc'] = 'VSC'
-# utci.groupby(['tsc', 'year']).count()
-
+months = [['January', 1], ['February', 2], ['March', 3], ['April', 4], ['May', 5], ['June', 6],
+          ['July', 7], ['August', 8], ['September', 9], ['October', 10], ['November', 11], ['December', 12]]
+months = pd.DataFrame(months, columns=['month', 'order'])
 
 # to know the min and max across all months
 data = pd.DataFrame()
-for i in range(1, 13):
-    temp = utci[utci['month'] == i].groupby(['lon', 'lat'])['utci'].mean().reset_index()
-    temp = temp.assign(month = i)
+for month in months['order'].unique():
+    temp = utci[utci['month'] == month].groupby(['lon', 'lat'])['utci'].mean().reset_index()
+    temp = temp.assign(month = month)
     data = pd.concat([data, temp])
-    print("Concat for month - " + str(i))
+    print("Concat for month - " + str(month))
 min = data['utci'].min()
 max = data['utci'].max()
 
 # to save maps on hdd
-months = [['January', 1], ['February', 2], ['March', 3], ['April', 4], ['May', 5], ['June', 6],
-          ['July', 7], ['August', 8], ['September', 9], ['October', 10], ['November', 11], ['December', 12]]
-months = pd.DataFrame(months, columns=['month', 'order'])
 for month in months['order'].unique():
     plot_hm(df=data, value='utci', vmax=max, vmin=min, month=month)
 
@@ -163,6 +135,9 @@ utci_slope = pd.read_pickle('results/slope_df_month_January.pkl')
 utci_slope['index'] = utci_slope['index'].astype(int)
 utci_slope.reset_index(drop=True, inplace=True)
 
+# calculate the p-value
+# the null-hypothesis is: slope_per_month is be the mean value in our dataset
+# if p-value < 0.05 then we reject the null-hypothesis
 pvalue_df = pd.DataFrame()
 for index in utci_slope["index"].unique():
     slope_per_month = utci_slope[utci_slope['index'] == index]['slope']
