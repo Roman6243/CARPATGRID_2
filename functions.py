e21f991ee661b51d4846d756d34e54c63cf34177
Roman6243 <mukhovskiy2012@gmail.com>
2021-02-25 20:37:01 +0100

    rewriting script as functions

--- functions.py
+++ functions.py
@@ -1,31 +1,31 @@
-def plot_hm_days(df, value, vmin, vmax, var):
-    df = pd.pivot_table(df, values=value, index=['lat'], columns=['lon'])
+def plot_hm_cat(df, value, vmin, vmax, variable):
+    df = pd.pivot_table(df, values=value, index=['lat'], columns=['lon'], dropna=False)
     df = df[::-1]
     plt.figure(figsize=(21, 11), dpi=100, facecolor='w', edgecolor='k')
     chart = sns.heatmap(df, vmin=vmin, vmax=vmax)
-    plt.savefig('maps/'+ cat + "_" + value + '.png')
-    print(" ".join(["save", value, "chart for category -", cat]))
+    plt.savefig('maps/'+ variable + "_" + value + '.png')
+    plt.close()
+    print(" ".join(["Save", value, "chart for category -", variable]))
 
 def plot_hm_utci(df, value, vmin, vmax, variable):
-    """
-    variable: int
-    """
     df = pd.pivot_table(df, values=value, index=['lat'], columns=['lon'])
     df = df[::-1]
     plt.figure(figsize=(21, 11), dpi=100, facecolor='w', edgecolor='k')
     chart = sns.heatmap(df, vmin=vmin, vmax=vmax)
     m_name = MONTHS[MONTHS['order'] == variable]['month'].tolist()[0]
     plt.savefig('maps/'+ str(variable) + "_" + m_name + "_" + value + '.png')
-    print(" ".join(["save", value, "chart for month -", m_name]))
+    plt.close()
+    print(" ".join(["Save", value, "chart for month -", m_name]))
 
 def read_file(ser_read, csv_write):
     global line_count
+    line_count = 0
     for row in ser_read:
         line_count += 1
         csv_write.write(";".join(row.split()) + "\n")
-    print(f'Read {line_count} rows.')
+    print(f'Write {line_count} rows.')
 
-def extract_d(f_input, f_output, param):
+def extract_data(f_input, f_output, param):
     if os.path.isfile(f_output):
         print("File " + f_output +  " exist")
     else:
@@ -69,5 +69,22 @@ def calculate_pvalue(df, var):
     pvalue_df.loc[pvalue_df['pvalue'] > 0.05, 'significance'] = "insignificance"
     pvalue_df.loc[pvalue_df['pvalue'] <= 0.05, 'significance'] = "significance"
     pvalue_df = pvalue_df.merge(alt, on=['index']).assign(variable = var)
-    print("\npvalue calculated successfully for category "+var+"\n")
+    print("\nPvalue calculated successfully for category "+var+"\n")
     return pvalue_df
+
+def read_altitude():
+    """
+    extract altitude
+    """
+    f_in = 'INPUT\\PredtandfilaGrid.dat'
+    f_out = 'INPUT\\altitude_grid.csv'
+    if os.path.isfile(f_out):
+        print('File ' + f_out + ' exists')
+    else:
+        print("File not exist")
+        with open(f_in, mode='r') as ser_read, open(f_out, mode='w') as csv_write:
+            line_count = 0
+            read_file(ser_read, csv_write)
+    alt = pd.read_csv(f_out, delimiter=';')
+    alt["index"] = alt["index"].astype(str)
+    return alt
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ plt.ioff()
 plt.isinteractive()
 
 os.chdir('C:\\Users\\mukho\\Downloads\\CARPATGRID')
-print(" ".join(['Current path is', os.getcwd()]))
+print(" ".join(['\nCurrent path is', os.getcwd()]))
 pd.set_option('display.max_columns', 300)
 pd.set_option('display.width', 1000)
 
@@ -25,29 +25,22 @@ MONTHS = pd.DataFrame(MONTHS, columns=['month', 'order'])
 YEARS = [[1961, 1970], [1971, 1980], [1981, 1990], [1991, 2000], [2001, 2010]]
 Y_FRAMES = pd.DataFrame(YEARS, columns=['start', 'end'])
 
-# extract altitude
-f_in = 'INPUT\\PredtandfilaGrid.dat'
-f_out = 'INPUT\\altitude_grid.csv'
-if os.path.isfile(f_out):
-    print('File ' + f_out + ' exist')
-else:
-    with open(f_in) as fin, open(f_out, 'w') as fout:
-        for line in fin:
-            fout.write(";".join(line.split()) + "\n")
-alt = pd.read_csv(f_out, delimiter=';')
-alt["index"] = alt["index"].astype(str)
+UTCI_RANGES_N = [-100, -40, -27, -13, 0, 9, 26, 32, 38, 46, 100]
+UTCI_RANGES_V = ['EC', 'VSC', 'SC', 'MC', 'SLC', 'NT', 'MH', 'SH', 'VSH', 'EH']
 
-print("\nWould you like to recalculate parameters: 1)RG; 2)MRT; 3)WS10m; 4)RH; 5)TMax ?\n")
-print("y/n ?")
-answer = input()
+alt = read_altitude()
 
-if answer == 'y':
+def calc_utci(alt):
+    """
+    calculate parameters: 1)RG; 2)MRT; 3)WS10m; 4)RH; 5)TMax
+    """
+
     # to write data in different year batch
     for i in range(5):
         start = Y_FRAMES['start'][i]
         end = Y_FRAMES['end'][i]
 
-        rg = extract_d(f_input = "CARPATGRID_RG\\CARPATGRID_RG_D.ser", f_output = "CARPATGRID_RG\\CARPATGRID_RG_D.csv", param = 'rg12')
+        rg = extract_data(f_input = "CARPATGRID_RG\\CARPATGRID_RG_D.ser", f_output = "CARPATGRID_RG\\CARPATGRID_RG_D.csv", param = 'rg12')
         rg = rg.merge(alt, how='left', on=['index'])
         rg.loc[rg['altitude'] >= 1200, 'rg12'] = -0.1945*(rg['rg12']**2) + 35.054*rg['rg12'] + 42.521
         rg.loc[rg['altitude'] < 1200, 'rg12'] = -0.3408*(rg['rg12']**2) + 39.654*rg['rg12'] + 23.76
@@ -56,17 +49,17 @@ if answer == 'y':
         mrt.loc[mrt['altitude'] >= 1200, 'mrt'] = -0.00002*(mrt['rg12']**2) + 0.0699*mrt['rg12'] - 13.387
         mrt.loc[mrt['altitude'] < 1200, 'mrt'] = -0.00003* (mrt['rg12']**2) + 0.0806*mrt['rg12'] - 7.1488
 
-        ws10m = extract_d(f_input = "CARPATGRID_WS10\\CARPATGRID_WS10_D.ser", f_output = "CARPATGRID_WS10\\CARPATGRID_WS10_D.csv", param = 'ws12')
+        ws10m = extract_data(f_input = "CARPATGRID_WS10\\CARPATGRID_WS10_D.ser", f_output = "CARPATGRID_WS10\\CARPATGRID_WS10_D.csv", param = 'ws12')
         ws10m = ws10m.merge(alt, how='left', on=['index'])
         ws10m.loc[ws10m['altitude'] >= 1200, 'ws12'] = ws10m['ws12']*1.7
         ws10m.loc[ws10m['altitude'] < 1200, 'ws12'] = ws10m['ws12']*1.4
         ws10m.loc[ws10m['ws12'] < 0.5, 'ws12'] = 0.5
         ws10m.loc[ws10m['ws12'] > 27, 'ws12'] = 27
 
-        rh = extract_d('CARPATGRID_RH\\CARPATGRID_RH_D.ser', "CARPATGRID_RH\\CARPATGRID_RH_D.csv", param = 'rh12')
+        rh = extract_data('CARPATGRID_RH\\CARPATGRID_RH_D.ser', "CARPATGRID_RH\\CARPATGRID_RH_D.csv", param = 'rh12')
         rh['rh12'] = rh['rh12'] * 0.86
 
-        tmax = extract_d(f_input = "CARPATGRID_TMAX\\CARPATGRID_TMAX_D.ser", f_output = "CARPATGRID_TMAX\\CARPATGRID_TMAX_D.csv", param = 't12')
+        tmax = extract_data(f_input = "CARPATGRID_TMAX\\CARPATGRID_TMAX_D.ser", f_output = "CARPATGRID_TMAX\\CARPATGRID_TMAX_D.csv", param = 't12')
 
         print("Merge all parameters:")
         print("Merge rh and ws10m")
@@ -83,55 +76,56 @@ if answer == 'y':
         # save calculated df on hdd
         df.to_pickle('results/df_' + str(start) + '_' + str(end) +'.pkl')
         print("\n***********************************************************************************************************************************************\n")
-else:
-    print("Skip this step of recalculation 1)RG; 2)MRT; 3)WS10m; 4)RH; 5)TMax")
 
-# utci_1961_1970 = pd.read_pickle('results/df_1961_1970.pkl')
-# utci_1971_1980 = pd.read_pickle('results/df_1971_1980.pkl')
-# utci_1981_1990 = pd.read_pickle('results/df_1981_1990.pkl')
-# utci_1991_2000 = pd.read_pickle('results/df_1991_2000.pkl')
-# utci_2001_2010 = pd.read_pickle('results/df_2001_2010.pkl')
-# 
-# utci_list = [utci_1961_1970, utci_1971_1980, utci_1981_1990, utci_1991_2000, utci_2001_2010]
-# utci = pd.concat(utci_list)
-# utci.to_pickle('results/df_' + str(1961) + '_' + str(2010) +'.pkl')
+    utci_1961_1970 = pd.read_pickle('results/df_1961_1970.pkl')
+    utci_1971_1980 = pd.read_pickle('results/df_1971_1980.pkl')
+    utci_1981_1990 = pd.read_pickle('results/df_1981_1990.pkl')
+    utci_1991_2000 = pd.read_pickle('results/df_1991_2000.pkl')
+    utci_2001_2010 = pd.read_pickle('results/df_2001_2010.pkl')
 
-utci = pd.read_pickle('results/df_1961_2010.pkl')
-print("UTCI data read from pickle files successfully!")
+    utci_list = [utci_1961_1970, utci_1971_1980, utci_1981_1990, utci_1991_2000, utci_2001_2010]
+    utci = pd.concat(utci_list)
+    utci.to_pickle('results/df_' + str(1961) + '_' + str(2010) +'.pkl')
 
+def read_utci():
+    """
+    read and calculate the number of days per each utci category
+    :return:
+    """
+    utci = pd.read_pickle('results/df_1961_2010.pkl')
+    for i in range(0, len(UTCI_RANGES_N)-1):
+        print("Define the category "+UTCI_RANGES_V[i] )
+        utci.loc[(utci['utci'] > UTCI_RANGES_N[i]) & (utci['utci'] <= UTCI_RANGES_N[i+1]), 'category'] = UTCI_RANGES_V[i]
+    print("UTCI data read from pickle files successfully!\n")
+    return utci
 
-# to know the min and max UTCI across all MONTHS
-data = pd.DataFrame()
-for month in MONTHS['order'].unique():
-    temp = utci[utci['month'] == month].groupby(['lon', 'lat'])['utci'].mean().reset_index()
-    temp = temp.assign(month = month)
-    data = pd.concat([data, temp])
-    print("Concat for month - " + str(month))
-min = data['utci'].min()
-max = data['utci'].max()
+def save_utci_map(utci):
+    """
+    calculate the min and max UTCI across all MONTHS in the aim to normalize the colors on heatmaps
+    """
+    data = pd.DataFrame()
+    for month in MONTHS['order'].unique():
+        temp = utci[utci['month'] == month].groupby(['lon', 'lat'])['utci'].mean().reset_index()
+        temp = temp.assign(month = month)
+        data = pd.concat([data, temp])
+        print(' '.join(["Append avg monthly utci data for month - ",month]))
+    min = data['utci'].min()
+    max = data['utci'].max()
+    for month in MONTHS['order'].unique():
+        data_m = data[data['month'] == month]
+        plot_hm_utci(df=data_m, value='utci', vmax=max, vmin=min, variable=month)
 
-# to save maps on hdd
-for month in MONTHS['order'].unique():
-    plot_hm_utci(df=data, value='utci', vmax=max, vmin=min, month=month)
-
-# there is no difference between average monthly and daily except February -> different number of days
-m_mean_daily = utci.groupby(['month'])['utci'].mean().reset_index()
-m_mean_monthly = utci.groupby(['year', 'month'])['utci'].mean().groupby(['month']).mean().reset_index()
-m_d_comp = m_mean_daily.merge(m_mean_monthly, on=['month'], suffixes = ['_daily_avg', '_monthly_avg'])
-m_d_comp['diff'] = round(m_d_comp['utci_daily_avg'] - m_d_comp['utci_monthly_avg'], 5)
-print(m_d_comp)
-
-# average daily temperature per year and calculate the trend (slope) of utci during 1961 - 2010 per each index
-utci_slope = utci[['year', 'month', 'index', 'utci']]
-
-print('\nWould you like to calculate slope (trend) per each month during whole time frame 1991-2010?\n')
-answer = input()
-if answer == 'y':
+def calc_slope_utci(utci):
+    """
+    average daily temperature per year and calculate the trend (slope) of utci during 1961 - 2010 per each index
+    """
     # for month_n in MONTHS['order'].unique():
-    for month_n in [2,3,4]:
+    # for that moment calculate only for three months
+    for month_n in [1,2,3]:
         slope_df = pd.DataFrame()
-        for index in utci_slope['index'].unique():
-            m_mean_year_monthly = utci_slope[(utci_slope['month'] == month_n) & (utci_slope['index'] == index)].groupby(['year'])['utci'].mean().reset_index()
+        df_month = utci[utci['month'] == month_n]
+        for index in utci['index'].unique():
+            m_mean_year_monthly = df_month[df_month['index'] == index].groupby(['year'])['utci'].mean().reset_index()
             x = m_mean_year_monthly['year'].to_numpy().reshape((-1,1))
             y = m_mean_year_monthly['utci'].to_numpy()
             model = LinearRegression(fit_intercept=True)
@@ -145,62 +139,91 @@ if answer == 'y':
             m_name = MONTHS[MONTHS['order'] == month_n]['month'].tolist()[0]
             print("calculate data for index - " + str(index))
         slope_df.to_pickle('results/utci_slope_df_month_' + str(m_name) + '.pkl')
-else:
-    print("Skip step of recalculation slope of UTCI per time frame 1961-2010\n")
 
-# read the slope data and calculate the p-value
-for m_number in [1,2,3]:
-    utci_slope = pd.read_pickle('results/utci_slope_df_month_'+MONTHS[MONTHS['order']==m_number]['month'].tolist()[0]+'.pkl')
-    utci_slope.reset_index(drop=True, inplace=True)
-    pvalue_df = calculate_pvalue(df=utci_slope, var=str(m_number))
-    plot_hm_utci(df=pvalue_df, value='pvalue', vmax=None, vmin=None, variable=m_number)
-    plot_hm_utci(df=pvalue_df, value='slope', vmax=None, vmin=None, variable=m_number)
+def read_slope_utci():
+    """
+    read the slope data and calculate the p-value
+    """
+    # for that moment calculate only for three months
+    for m_number in [1,2,3]:
+        utci_slope = pd.read_pickle('results/utci_slope_df_month_'+MONTHS[MONTHS['order']==m_number]['month'].tolist()[0]+'.pkl')
+        utci_slope.reset_index(drop=True, inplace=True)
+        pvalue_df = calculate_pvalue(df=utci_slope, var=str(m_number))
+        plot_hm_utci(df=pvalue_df, value='pvalue', vmax=None, vmin=None, variable=m_number)
+        plot_hm_utci(df=pvalue_df, value='slope', vmax=None, vmin=None, variable=m_number)
 
 # *********************************** CATEGORY CALCULATION ***********************************
-# calculate the number of days per each utci category
-utci_ranges_n = [min, -40, -27, -13, 0, 9, 26, 32, 38, 46, max]
-utci_ranges_v = ['EC', 'VSC', 'SC', 'MC', 'SLC', 'NT', 'MH', 'SH', 'VSH', 'EH']
-for i in range(0, len(utci_ranges_n)-1):
-    print("Define the category "+utci_ranges_v[i] )
-    utci.loc[(utci['utci'] > utci_ranges_n[i]) & (utci['utci'] <= utci_ranges_n[i+1]), 'category'] = utci_ranges_v[i]
+# ********************************************************************************************
+# Linear trend for the annual number of days of thermal stress categories
+# ********************************************************************************************
 
-print('\nWould you like to calculate slope (trend) per decade during 1991-2010?\n')
-answer = input()
-if answer == 'y':
-    for i in range(1,5):
-        df = utci[(utci['year']>=Y_FRAMES['start']) & (utci['year'] <= Y_FRAMES['end'])]
-        slope_days = pd.DataFrame()
-        for index in df['index'].unique():
-            df_dec_index = df[df['index'] == index].groupby(['year', 'category'])['utci'].\
-                count().reset_index().rename(columns={'utci':'n_days'})
-            temp_inter = pd.DataFrame()
+def calc_slope_days_cat(utci):
+    """
+    to calculate slope (trend) of days number category during 1961-2010
+    """
+    slope_days = pd.DataFrame()
+    # for cat in utci['category'].unique():
+    # for that moment calculate only for current category
+    for cat in ['SC', 'VSH', 'VSC', 'EC', 'EH']:
+        df_cat = utci[utci['category'] == cat]
+        temp_cat = pd.DataFrame()
+        for index in sorted(df_cat['index'].unique()):
+            df_cat_index = df_cat[df_cat['index'] == index].groupby(['year', 'category'])['utci']. \
+                count().reset_index().rename(columns={'utci': 'n_days'})
 
-            for cat in df_dec_index['category'].unique():
-                df_annual_slope_dec = df_dec_index[df_dec_index['category'] == cat]
+            x = df_cat_index['year'].to_numpy().reshape((-1,1))
+            y = df_cat_index['n_days'].to_numpy()
+            model = LinearRegression(fit_intercept=True)
+            model.fit(x, y)
+            slope = round(model.coef_[0], 5)
 
-                x = df_annual_slope_dec['year'].to_numpy().reshape((-1,1))
-                y = df_annual_slope_dec['n_days'].to_numpy()
-                model = LinearRegression(fit_intercept=True)
-                model.fit(x, y)
-                slope = round(model.coef_[0], 5)
+            temp = [[index, slope, cat]]
+            temp = pd.DataFrame(temp, columns=['index', 'slope', 'category'])
+            temp_cat = pd.concat([temp_cat, temp])
+            print(" ".join(["calculate data for category -", cat, "and for index", index]))
+        slope_days = pd.concat([slope_days, temp_cat])
+        slope_days.to_pickle("results/days_slope_df_1961_2010_"+cat+".pkl")
 
-                temp = [[index, str(start)+"::" + str(end), slope, cat]]
-                temp = pd.DataFrame(temp, columns=['index', 'decade', 'slope', 'category'])
-                temp_inter = pd.concat([temp_inter, temp])
+def read_slope_days_cat(utci):
+    days_slope = pd.DataFrame()
+    for cat in utci['category'].unique():
+        temp = pd.read_pickle("results/days_slope_df_1961_2010_"+cat+".pkl")
+        days_slope = pd.concat([days_slope, temp])
+    return days_slope
 
-            slope_days = pd.concat([slope_days, temp_inter])
-            print("calculate data for index - " + str(index))
-        slope_days.to_pickle("results/days_slope_df_"+str(Y_FRAMES['start'])+".pkl")
+def save_slope_pvalue_map(days_slope):
+    for cat in days_slope['category'].unique():
+        df_cat = days_slope[days_slope['category'] == cat]
+        df_cat = calculate_pvalue(df=df_cat, var=cat)
+        df_cat = df_cat.merge(alt[['lon', 'lat']], on=['lon', 'lat'], how='right')
+        df_cat.to_excel("maps/n_days_slope/" + cat + '_n_days_slope.xlsx')
+        plot_hm_cat(df=df_cat, value='pvalue', vmin=None, vmax=None, variable=cat)
+        plot_hm_cat(df=df_cat, value='slope', vmin=None, vmax=None, variable=cat)
+
+# ********************************************************************************************
+# Mean annual number of days of thermal stress categories
+# ********************************************************************************************
+def save_days_cat_map(utci):
+    for cat in utci['category'].unique():
+        df_cat = utci[utci['category']==cat]
+        df_cat = df_cat.groupby(['year', 'lon', 'lat']).count().reset_index().\
+            groupby(['lon', 'lat'])['category'].mean().reset_index().\
+            rename(columns={'category':'n_days'})
+        df_cat = df_cat.merge(alt[['lon', 'lat']], on=['lon', 'lat'], how='right')
+        df_cat.to_excel("maps/n_days_per_cat/"+cat+'_n_dasys.xlsx')
+        plot_hm_cat(df=df_cat, value='n_days', vmin=None, vmax=None, variable=cat)
+
+def main():
+    utci = read_utci()
+    save_days_cat_map(utci)
+    # days_slope = read_slope_days_cat(utci)
+    # save_slope_pvalue_map(days_slope)
+
+if __name__ == "__main__":
+    main()
 
 
-days_slope = pd.read_pickle("results/days_slope_df.pkl")
-for cat in days_slope['category'].unique():
-    df_cat = days_slope[days_slope['category'] == cat]
-    df_cat = calculate_pvalue(df=df_cat, var=cat)
-    plot_hm_days(df=df_cat, value='pvalue', vmin=None, vmax=None, var=cat)
-    plot_hm_days(df=df_cat, value='slope', vmin=None, vmax=None, var=cat)
 
-# ************************************************************************************************************************
 # Density Plot and Histogram of all arrival delays
 # sns.distplot(utci_slope['slope'], hist=True, kde=True,
 #              bins=200, color = 'darkblue',
@@ -212,3 +235,4 @@ for cat in days_slope['category'].unique():
 # sns.boxplot(x=utci_slope['slope'])
 # plt.show()
 # plt.close()
+# def main():
